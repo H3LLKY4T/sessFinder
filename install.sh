887cdbe1d1eeaa6ff40729f1999d5d2e81017cf6
Joel Patrick <141442170+H3LLKY4T@users.noreply.github.com>
2024-03-12 12:11:35 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+INSTALL_DIR="/usr/local/bin"
+RESOURCES_DIR="./Resources"
+SCRIPT_NAME="sessFinder" # change this to your script's name
+SCRIPT_PATH="$RESOURCES_DIR/sessFinder.sh" # Adjust the script name/path as necessary
+
+if [ ! -d "$RESOURCES_DIR" ]; then
+  echo "The Resources directory does not exist. Please run this script from the correct location."
+  exit 1
+fi
+
+cp "$SCRIPT_PATH" "$INSTALL_DIR/$SCRIPT_NAME"
+
+chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
+
+echo "Installation completed. You can now run the script using '$SCRIPT_NAME'"
+
+if [ "$1" == "--uninstall" ]; then
+  rm -f "$INSTALL_DIR/$SCRIPT_NAME"
+  echo "Uninstallation completed."
+  exit 0
+fi
+
